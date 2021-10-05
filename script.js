1699b5e729696a961a2cf23a56cc11a0af80921e
Kirill Korshunov <kkv2410@gmail.com>
2021-10-05 22:42:58 +0700

    Feat(script.js): assigning values ​​to variableand output on display

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'MyRepo';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 2500;
+let rollback = 70;
+let fullPrice = 10000;
+let adaptive = true;
 
-alert("Hello world!");
+// alert("Hello world!");
 
 console.log('First console message');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLowerCase());
+
+console.log(fullPrice * (rollback/100));
+
+
