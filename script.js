2c2e03a2205a69a19544d862bb2a3c127fe0d1ac
Kirill Korshunov <kkv2410@gmail.com>
2021-10-07 21:03:58 +0700

    Feat (script.js): Add computation from lesson № 3

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const title = 'MyRepo';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 2500;
-const rollback = 70;
-const fullPrice = 10000;
-const adaptive = true;
+let title = 'MyRepo';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 2500;
+const rollback = 68;
+let fullPrice = 10000;
+let adaptive = true;
 
-alert("Hello world!");
+// alert("Hello world!");
 
 console.log('First console message');
 
@@ -22,4 +22,45 @@ console.log(screens.toLowerCase());
 
 console.log(fullPrice * (rollback/100));
 
+title = prompt("Как называется ваш проект?");
+console.log(title);
 
+screens = prompt("Какие типы экранов нужно разработать?");
+console.log(screens);
+
+screenPrice = prompt("Сколько будет стоить данная работа?");
+console.log(screenPrice);
+
+adaptive = !!prompt("Нужен ли адаптив на сайте?");
+console.log(adaptive);
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service1);
+
+let servicePrice1 = prompt("Сколько это будет стоить?");
+console.log(servicePrice1);
+
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service2);
+
+let servicePrice2 = prompt("Сколько это будет стоить?");
+console.log(servicePrice2);
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+    console.log("Даем скидку в 10%"); 
+} else {
+    if (fullPrice >= 15000 && fullPrice < 30000) {
+        console.log("Даем скидку в 5%"); 
+    } else {
+        if (fullPrice < 15000 && fullPrice >= 0) {
+            console.log("Скидка не предусмотрена"); 
+        } else {
+            console.log("Что-то пошло не так");
+        }
+    }
+}
