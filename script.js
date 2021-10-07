2eca0cfe013469aacbd5e07548cab61302510f94
Kirill Korshunov <kkv2410@gmail.com>
2021-10-07 21:56:58 +0700

    Fix (script.js): fix variables type and delete unless variables

--- script.js
+++ script.js
@@ -1,54 +1,23 @@
-let title = 'MyRepo';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 2500;
 const rollback = 68;
-let fullPrice = 10000;
-let adaptive = true;
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?");
+const screenPrice = prompt("Сколько будет стоить данная работа?");
+const adaptive = !!prompt("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = prompt("Сколько это будет стоить?");
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
 
-// alert("Hello world!");
-
-console.log('First console message');
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей");
-
-console.log(screens.toLowerCase());
-
-console.log(fullPrice * (rollback/100));
-
-title = prompt("Как называется ваш проект?");
 console.log(title);
-
-screens = prompt("Какие типы экранов нужно разработать?");
 console.log(screens);
-
-screenPrice = prompt("Сколько будет стоить данная работа?");
 console.log(screenPrice);
-
-adaptive = !!prompt("Нужен ли адаптив на сайте?");
 console.log(adaptive);
-
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
 console.log(service1);
-
-let servicePrice1 = prompt("Сколько это будет стоить?");
 console.log(servicePrice1);
-
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
 console.log(service2);
-
-let servicePrice2 = prompt("Сколько это будет стоить?");
 console.log(servicePrice2);
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
 console.log(servicePercentPrice);
 
 if (fullPrice >= 30000) {
