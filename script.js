ce20f7cad1185cd566ef5ad133b66193e97971e2
Kirill Korshunov <kkv2410@gmail.com>
2021-10-20 16:20:15 +0700

    Refactor (script.js): fixed methods like in lesson's video

--- script.js
+++ script.js
@@ -2,58 +2,74 @@
 
 const appData = {
     title: "",
-    screens: "",
+    screens: [],
     screenPrice: 0,
     adaptive: true,
     rollback: 10,
     allServicePrices: 0,
     fullPrice: 0,
     servicePercentPrice: 0,
-    service1: "",
-    service2: "",
+    services: {},
     start: function () {
         appData.asking();
-        appData.allServicePrices = appData.getAllServicePrices();
-        appData.fullPrice = appData.getFullPrice();
-        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
-        appData.title = appData.getTitle();
+        appData.addPrices();
+        appData.getFullPrice();
+        appData.getServicePercentPrice();
+        appData.getTitle();
         appData.logger();
     },
 
     asking: function () {
         appData.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
-        appData.screens = prompt("Какие типы экранов нужно разработать?", "Простые, сложные");
-        appData.screenPrice = +prompt("Сколько будет стоить данная работа?");
-        while (!isNumber(appData.screenPrice)) {
-            appData.screenPrice = +prompt("Сколько будет стоить данная работа?");
+
+        for (let i = 0; i < 2; i++) {
+            let name = prompt("Какие типы экранов нужно разработать?", "Простые, сложные");
+            let price = +prompt("Сколько будет стоить данная работа?");
+
+            while (!isNumber(price)) {
+                price = +prompt("Сколько будет стоить данная работа?");
+            }
+
+            appData.screens.push({
+                id: i,
+                name: name,
+                price: price
+            });
         }
+
+        for (let i = 0; i < 2; i++) {
+            let name = prompt("Какой дополнительный тип услуги нужен?");
+            let price = 0;
+            price = prompt("Сколько это будет стоить?");
+            while (!isNumber(price)) {
+                price = prompt("Сколько это будет стоить?");
+            }
+            appData.services[name] = +price;
+        }
+
         appData.adaptive = confirm("Нужен ли адаптив на сайте?");
     },
 
-    getAllServicePrices: function () {
-        let sum = 0;
-        let price;
-        for (let i = 0; i < 2; i++) {
-            if (i === 0) {
-                appData.service1 = prompt("Какой дополнительный тип услуги нужен?");
-            } else if (i === 1) {
-                appData.service2 = prompt("Какой дополнительный тип услуги нужен?");
-            }
-            price = +prompt("Сколько это будет стоить?");
-            while (!isNumber(price)) {
-                price = +prompt("Сколько это будет стоить?");
-            }
-            sum += price;
+    addPrices: function () {
+        for (let screen of appData.screens) {
+            appData.screenPrice += +screen.price;
         }
-        return sum;
+
+        for (let key in appData.services) {
+            appData.allServicePrices += appData.services[key];
+        }
     },
 
     getFullPrice: function () {
-        return appData.screenPrice + appData.allServicePrices;
+        appData.fullPrice = appData.screenPrice + appData.allServicePrices;
+    },
+
+    getServicePercentPrice: function () {
+        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
     },
 
     getTitle: function (str) {
-        return appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase();
+        appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase();
     },
 
     getRollbackMessage: function (price) {
@@ -73,6 +89,10 @@ const appData = {
     },
 
     logger: function () {
+        console.log(appData.fullPrice);
+        console.log(appData.servicePercentPrice);
+        console.log(appData.screens);
+
         for (let prop in appData) {
             console.log(prop + " = " + appData[prop]);
         }
