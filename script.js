9c978e0fede99f32646b45ec09c9ce8ffe45fb77
Kirill Korshunov <kkv2410@gmail.com>
2021-10-20 16:27:45 +0700

    Feature (script.js): add data type checking for request

--- script.js
+++ script.js
@@ -21,11 +21,16 @@ const appData = {
 
     asking: function () {
         appData.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+        while (isNumber(appData.title)) {
+            appData.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+        }
 
         for (let i = 0; i < 2; i++) {
             let name = prompt("Какие типы экранов нужно разработать?", "Простые, сложные");
+            while (isNumber(appData.title)) {
+                name = prompt("Какие типы экранов нужно разработать?", "Простые, сложные");
+            }
             let price = +prompt("Сколько будет стоить данная работа?");
-
             while (!isNumber(price)) {
                 price = +prompt("Сколько будет стоить данная работа?");
             }
@@ -39,6 +44,9 @@ const appData = {
 
         for (let i = 0; i < 2; i++) {
             let name = prompt("Какой дополнительный тип услуги нужен?");
+            while (isNumber(appData.title)) {
+                name = prompt("Какой дополнительный тип услуги нужен?");
+            }
             let price = 0;
             price = prompt("Сколько это будет стоить?");
             while (!isNumber(price)) {
